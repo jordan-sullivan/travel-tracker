ba133335192d890c38c8449e070a5aeeb42af8b0
Jordan Sullivan <helloandhi.jp@gmail.com>
2022-06-11 15:47:57 -0600

    update html with images and beginnig layout

--- src/scripts.js
+++ src/scripts.js
@@ -2,11 +2,11 @@
 // Do not delete or rename this file ********
 
 // An example of how you tell webpack to use a CSS (SCSS) file
-import dayjs from 'dayjs';
+//import dayjs from 'dayjs';
 import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/sands.jpg'
 import dayjs from 'dayjs';
 dayjs().format();
 
