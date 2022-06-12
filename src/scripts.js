45c8c69e3af03afea13ad6588d5ab5380f5812bd
Jordan Sullivan <helloandhi.jp@gmail.com>
2022-06-12 16:56:21 -0600

    create class to connect methods to DOM

--- src/scripts.js
+++ src/scripts.js
@@ -6,42 +6,61 @@ import { fetchAll } from "./api-calls.js";
 import Traveler from "./classes/Traveler.js";
 import Trip from "./classes/Trip.js";
 
-// Global Variables //
-//let travelersData, destinationsData, tripsData;
+// - - - - - Global Variables - - - - - //
+let travelersData, destinationsData, tripsData;
 
-//Event listeners//
+// - - - - - Query Selectors - - - - - //
+const pastTripsDisplay = document.querySelector(".past-trips")
+const currentTripsDisplay = document.querySelector(".current-trips")
+const upcomingTripsDisplay = document.querySelector(".upcoming-trips")
+const yearlySpending = document.querySelector(".yearly-spending")
+
+// - - - - - - Event listeners - - - - - //
 window.addEventListener("load", () => {
     getAllData();
+    displayYearlyTripCost();
 });
 
 const getAllData = () => {
         fetchAll()
             .then(data => {
                const travelersData = data[0].travelers;
-               console.log("TRAVELERS DATA", travelersData)
-
+               //console.log("TRAVELERS DATA", travelersData)
                 const tripsData = data[1].trips;
-                console.log("TRIPS DATA", tripsData)
-
+                //console.log("TRIPS DATA", tripsData)
                 const destinationsData = data[2].destinations;
-                console.log("DESTINATIONS DATA", destinationsData)
+                //console.log("DESTINATIONS DATA", destinationsData)
 
                 //const singleTravelerData = data[3].travelers;
                 //console.log("SINGLE TRAVELERS DATA", singleTravelerData)
 
-                //want to make each a new instanciation of 
+                //want to make each a new instanciation 
 
-                //const traveler = new Traveler(travelersData)
-                //console.log("traveleRR", traveler)//just a sad object of undefined-ness
+                const traveler = new Traveler(travelersData)
+                console.log("traveleRR", traveler)//just a sad object of undefined-ness
 
                 const travelers = travelersData.map(trav => new Traveler(trav));
-                console.log("allTravelers", travelers) // array of all 50 Travelers
-
-                //const trips = tripsData.map(trip => new Trip(trip));
-                //console.log("allTrips", trips);
+                //console.log("allTravelers", travelers) // array of all 50 Travelers
+                
+                const trips = tripsData.map(trip => new Trip(trip));
+                console.log("allTrips", trips);
+                console.log("T.TTD", tripsData.trip.travelerTripData)
 
                 //const allDestinations = destinationsData.map(dest => new Destination(dest));
                 //console.log("allDestinations", allDestinations)
             })
     }
+
+const loadTravelerInfo(traveler) {
+    welcomeMessage.innerText = `Hi, ${traveler.returnFirstName()},`;
+    todaysDate.innerText = `${dayjs(Date.now()).format('ddd MMM D YYYY')}`;
+    displayYearlyTripCost();
+};
+
+
+const displayYearlyTripCost = () => {
+    yearlySpending.innerText = `$${tripsData.getYearsTripCost(destinationsData)}`;
+};
+
+
 
