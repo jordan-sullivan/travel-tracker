fe6e8e79641cc88730263f4ab6bb9314930cc54d
Jordan Sullivan <helloandhi.jp@gmail.com>
2022-06-12 20:59:57 -0600

    add instanciation to data

--- src/scripts.js
+++ src/scripts.js
@@ -7,7 +7,7 @@ import Traveler from "./classes/Traveler.js";
 import Trip from "./classes/Trip.js";
 
 // - - - - - Global Variables - - - - - //
-let travelersData, destinationsData, tripsData;
+let travelers, currentTraveler, trips, travelersData, destinationsData, tripsData;
 
 // - - - - - Query Selectors - - - - - //
 const pastTripsDisplay = document.querySelector(".past-trips")
@@ -17,50 +17,64 @@ const yearlySpending = document.querySelector(".yearly-spending")
 
 // - - - - - - Event listeners - - - - - //
 window.addEventListener("load", () => {
+    //makeRandomUser();
     getAllData();
-    displayYearlyTripCost();
+    //displayYearlyTripCost();
 });
 
+ const makeRandomUser = (travelersData) => {
+     console.log("RANDTRAVDATA", travelersData)
+    //travelers = travelersData.map(traveler => new Traveler(travelersData));
+    currentTraveler = travelers[Math.floor(Math.random() * travelersData.length)];
+};
+// return a current traveler (Global?) has an ids
+// currentTrav.id  pass this thoruhg as trip user ID 
+console.log(currentTraveler)
+
 const getAllData = () => {
-        fetchAll()
+        fetchAll() 
             .then(data => {
-               const travelersData = data[0].travelers;
-               //console.log("TRAVELERS DATA", travelersData)
+                const travelersData = data[0].travelers;
+                //console.log("TRAVELERS DATA", travelersData)
+
                 const tripsData = data[1].trips;
                 //console.log("TRIPS DATA", tripsData)
+
                 const destinationsData = data[2].destinations;
                 //console.log("DESTINATIONS DATA", destinationsData)
+                //take out of .then, and do cascading functions after 
+                //id needs to be acceessible everywhere i go 
 
                 //const singleTravelerData = data[3].travelers;
                 //console.log("SINGLE TRAVELERS DATA", singleTravelerData)
 
-                //want to make each a new instanciation 
+                // const singleTraveler = new Traveler(trip.travelerTripData))
+                // console.log("traveleRR", singleTraveler)//just a sad object of undefined-ness
 
-                const traveler = new Traveler(travelersData)
-                console.log("traveleRR", traveler)//just a sad object of undefined-ness
-
-                const travelers = travelersData.map(trav => new Traveler(trav));
-                //console.log("allTravelers", travelers) // array of all 50 Travelers
+                travelers = travelersData.map(trav => new Traveler(trav));
+                console.log("allTravelers", travelers) // array of all 50 Travelers
                 
-                const trips = tripsData.map(trip => new Trip(trip));
-                console.log("allTrips", trips);
-                console.log("T.TTD", tripsData.trip.travelerTripData)
+                trips = tripsData.map(trip => new Trip(tripsData));
+                // console.log("travTriipsData", travelerTripData);
+                console.log("Trips", trips)
 
                 //const allDestinations = destinationsData.map(dest => new Destination(dest));
                 //console.log("allDestinations", allDestinations)
-            })
+                //make new Destination class. 
+
+            });
     }
 
-const loadTravelerInfo(traveler) {
-    welcomeMessage.innerText = `Hi, ${traveler.returnFirstName()},`;
-    todaysDate.innerText = `${dayjs(Date.now()).format('ddd MMM D YYYY')}`;
-    displayYearlyTripCost();
-};
+// const loadTravelerInfo = (traveler) => {
+//     welcomeMessage.innerText = `Hi, ${traveler.returnFirstName()},`;
+//     todaysDate.innerText = `${dayjs(Date.now()).format('ddd MMM D YYYY')}`;
+//     displayYearlyTripCost();
+// };
 
 
-const displayYearlyTripCost = () => {
-    yearlySpending.innerText = `$${tripsData.getYearsTripCost(destinationsData)}`;
-};
+// const displayYearlyTripCost = () => {
+//     yearlySpending.innerText = `$${tripsData.getYearsTripCost(destinationsData)}`;
+// };
 
 
 
