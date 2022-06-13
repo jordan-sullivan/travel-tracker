ce12dff72f316eea71924e016c86198bb6aae67c
Jordan Sullivan <helloandhi.jp@gmail.com>
2022-06-13 17:44:26 -0600

    refactor classes for srp

--- src/classes/Destination.js
+++ src/classes/Destination.js
@@ -1,13 +1,23 @@
+// class Destination {
+//     constructor(destinationData) {
+//         this.id = destinationData.id;
+//         this.destination = destinationData.destination;
+//         this.estimatedLodgingCostPerDay = destinationData.estimatedLodgingCostPerDay;
+//         this.estimatedFlightCostPerPerson = destinationData.estimatedFlightCostPerPerson;
+//         this.image = destinationData.image;
+//         this.alt = destinationData.alt;
+//     };
+// };
 class Destination {
-    constructor(destinationData) {
-        this.id = destinationData.id;
-        this.destination = destinationData.destination;
-        this.estimatedLodgingCostPerDay = destinationData.estimatedLodgingCostPerDay;
-        this.estimatedFlightCostPerPerson = destinationData.estimatedFlightCostPerPerson;
-        this.image = destinationData.image;
-        this.alt = destinationData.alt;
-    };
-};
+    constructor(singleDest) {
+        this.id = singleDest.id;
+        this.destination = singleDest.destination;
+        this.estimatedLodgingCostPerDay = singleDest.estimatedLodgingCostPerDay;
+        this.estimatedFlightCostPerPerson = singleDest.estimatedFlightCostPerPerson;
+        this.image = singleDest.image;
+        this.alt = singleDest.alt;
+    }
+}
 
 
 
