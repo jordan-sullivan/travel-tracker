a218af1c89817c93780f098fdde1249d2289dc46
Jordan Sullivan <helloandhi.jp@gmail.com>
2022-06-13 14:19:04 -0600

    restructure network requests with fetch

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,96 +1,25 @@
-//Global Variables//
-let apiTravelersData, apiTripsData, apiDestinationsData;
 
-//Query Selectors//
-//const postError = document.querySelector(".error");
+const fetchAPIData = (dataSet) => {
+        return fetch(`http://localhost:3001/api/v1/${dataSet}`)
+            .then((response) => response.json())
+            //.catch((error) => console.log(error))
+    };
 
-//Functions//
-const fetchData = (dataSet) => {
-    return fetch(`http://localhost:3001/api/v1/${dataSet}`)
-        .then((response) => response.json())
-        //.catch((error) => console.log(dataSet))
-};
+    const fetchSingleTraveler = (id) => {
+            return fetch(`http://localhost:3001/api/v1/travelers/${id}`)
+            .then((response) => response.json())
+                .catch(err => console.log(err))
+        }
+        
+    export const fetchAll = (id) => {
+            return Promise.all([
+                fetchAPIData("travelers"),
+                fetchAPIData("trips"),
+                fetchAPIData("destinations"),
+                fetchSingleTraveler(`${id}`)
+                ]);
+                  .catch (err => console.log(err))
+                 };
+                
 
-export const fetchAll = (randomUserID) => {
-    apiTravelersData = fetchData("travelers");
-    apiTripsData = fetchData("trips");
-    apiDestinationsData = fetchData("destinations");
-    //apiSingleTravelerData = fetchData("travelers/23");
-    return Promise.all([
-        apiTravelersData,
-        apiTripsData,
-        apiDestinationsData,
-        //apiSingleTravelerData,
-    ]);
-};
-
-// export const postAll = (formData => {
-//     postHydration(formData);
-//     postSleep(formData);
-//     postActivity(formData);
-// });
-
-// const postHydration = (formData) => {
-//     fetch("http://localhost:3001/api/v1/hydration", {
-//         method: "POST",
-//         body: JSON.stringify({
-//             userID: formData.id, date: formData.date, numOunces: formData.numberOunces
-//         }),
-//         headers: { "Content-type": "application/json" },
-//     })
-//         .then(res => throwError(res))
-//         .then(json => reloadData())
-//         .catch(error => {
-//             console.warn(error.message)
-//             displayErrorMessage(error)
-//         })
-// };
-
-// const postSleep = (formData) => {
-//     fetch("http://localhost:3001/api/v1/sleep", {
-//         method: "POST",
-//         body: JSON.stringify({
-//             userID: formData.id, date: formData.date, hoursSlept: formData.hoursSlept, sleepQuality: formData.sleepQuality
-//         }),
-//         headers: { "Content-type": "application/json" },
-//     })
-//         .then(res => throwError(res))
-//         .then(json => reloadData())
-//         .catch(error => {
-//             console.warn(error.message)
-//             displayErrorMessage(error)
-//         })
-// };
-
-// const postActivity = (formData) => {
-//     fetch("http://localhost:3001/api/v1/activity", {
-//         method: "POST",
-//         body: JSON.stringify({
-//             userID: formData.id, date: formData.date, flightsOfStairs: formData.flights, minutesActive: formData.mins,
-//             numSteps: formData.steps
-//         }),
-//         headers: { "Content-type": "application/json" },
-//     })
-//         .then(res => throwError(res))
-//         .then(json => reloadData())
-//         .catch(error => {
-//             console.warn(error.message)
-//             displayErrorMessage(error)
-//         })
-// };
-
-// const throwError = (res) => {
-//     if (!res.ok) {
-//         throw new Error("Please make sure all fields are filled out.");
-//     } else {
-//         return res.json();
-//     };
-// };
-
-// const displayErrorMessage = (error) => {
-//     if (error.message === "Failed to fetch") {
-//         return postError.innerText = "OOPS something went wrong";
-//     } else {
-//         return postError.innerText = error.message;
-//     };
-//};
+export { fetchAll }
