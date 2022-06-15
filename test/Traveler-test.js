f4b34c1fbec71d7f0f84bbfed35a81c734a68aa1
Jordan Sullivan <95270427+jordan-sullivan@users.noreply.github.com>
2022-06-14 18:45:21 -0600

    adjust Traveler tests to reflect new method adjustments

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -1,10 +1,11 @@
 const expect = chai.expect;
 import chai from 'chai';
 import Traveler from "../src/classes/Traveler";
-import { travelersData} from "./sample-test-data"
+// import Destination from "./Destination"
+import { travelersData, tripData, destinationsData} from "./sample-test-data"
 
 describe ("Traveler", () => {
-    let traveler20, traveler21, traveler22, traveler23;
+    let traveler20, traveler21, traveler22, traveler23, date;
 
     beforeEach(() => {
         traveler20 = new Traveler(travelersData[0]);
@@ -28,10 +29,64 @@ describe ("Traveler", () => {
         expect(traveler23.travelerType).to.equal("thrill-seeker");
     });
 
-    it("should be able to return a traveler's first name", () => {
-        expect(traveler20.returnFirstName()).to.equal("Gregg");
-        expect(traveler23.returnFirstName()).to.equal("Welsh");
-        expect(traveler22.returnFirstName()).to.equal("Gus");
+    it("should store a different single traveler's information", () => {
+        expect(traveler22.id).to.deep.equal(22);
+        expect(traveler22.name).to.equal("Gus Courtenay");
+        expect(traveler22.travelerType).to.equal("foodie");
     });
 
+    it("should store a username for each traveler logging in", () => {
+        expect(traveler20.username).to.equal("traveler20");
+        expect(traveler21.username).to.equal("traveler21");
+        expect(traveler22.username).to.equal("traveler22");
+    });
+
+    it("should be able to return a traveler's past trips", () => {
+        date = "2022/06/10"
+        traveler20.findTrips(tripData, destinationsData);
+        const previousTrips = traveler20.findPastTrips(date)
+        expect(previousTrips).to.deep.equal([
+            {
+            "date": "2020/09/06",
+            "destinationID": 15,
+            "duration": 0,
+            "endDateTimeStamp": 1599372000000,
+            "id": 45,
+            "startDateTimeStamp": 1599372000000,
+            "status": "approved",
+            "suggestedActivities": [],
+            "travelers": 1,
+            "tripCost": 990,
+            "userID": 20,
+         }]
+      )
+    });
+
+    it("should be able to return a traveler's present trips", () => {
+        date = "2022/06/10"
+        traveler20.findTrips(tripData, destinationsData);
+        const pastTrips = traveler20.findPresentTrips(date)
+        expect(pastTrips).to.deep.equal([])
+    });
+
+    it("should be able to return a traveler's upcoming trips", () => {
+        date = "2022/06/10"
+        traveler23.findTrips(tripData, destinationsData);
+        const upcomingTrips = traveler23.findUpcomingTrips(date)
+        expect(upcomingTrips).to.deep.equal([
+            {
+            "date": "2022/07/14",
+            "destinationID": 21,
+            "duration": 19,
+            "endDateTimeStamp": 1659420000000,
+            "id": 56,
+            "startDateTimeStamp": 1657778400000,
+            "status": "pending",
+            "suggestedActivities": [],
+            "travelers": 3,
+            "tripCost": 3245,
+            "userID": 23,
+         }
+      ])
+    });
 })
